47dc27dc37e90223e00af9838ee281f60ee1852a
Ángel Píñar Rivas <angle@correo.ugr.es>
2020-10-26 14:59:36 +0100

    #12 Incluido esqueleto para componente de notas

---
+++ src/notes/note.go
@@ -0,0 +1,18 @@
+package notes
+
+type Note struct {
+	id int
+	note string
+}
+
+func (n Note) load() {
+
+}
+
+func (n Note) save() {
+
+}
+
+func (n Note) remove() {
+	
+}
---
+++ src/notes/notemanager.go
@@ -0,0 +1,25 @@
+package notes
+
+type NoteManager struct {
+
+}
+
+func (n NoteManager) getNote(){
+
+}
+
+func (n NoteManager) getNotes(){
+
+}
+
+func (n NoteManager) newNote(){
+
+}
+
+func (n NoteManager) editNote(){
+
+}
+
+func (n NoteManager) deleteNote(){
+	
+}
