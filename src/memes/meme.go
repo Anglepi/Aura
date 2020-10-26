985fa0e5abe592e81c7183d838e14ac66c69e114
Ángel Píñar Rivas <angle@correo.ugr.es>
2020-10-26 14:59:01 +0100

    #13 Incluidos esqueletos para componente de memes

---
+++ src/memes/meme.go
@@ -0,0 +1,18 @@
+package memes
+
+type Meme struct {
+	id int
+	imagePath string
+}
+
+func (m Meme) load(){
+
+}
+
+func (m Meme) save(){
+
+}
+
+func (m Meme) delete(){
+
+}
---
+++ src/memes/mememanager.go
@@ -0,0 +1,21 @@
+package memes
+
+type MemeManager struct {
+
+}
+
+func (m MemeManager) getMeme(){
+
+}
+
+func (m MemeManager) getRandomMeme(){
+
+}
+
+func (m MemeManager) newMeme(){
+
+}
+
+func (m MemeManager) deleteMeme(){
+	
+}
