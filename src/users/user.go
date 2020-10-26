912623e7bb9b2c13f58eb4ec97749e0c119ea8fa
Ángel Píñar Rivas <angle@correo.ugr.es>
2020-10-26 15:00:35 +0100

    #10 Incluido esqueleto de componente de usuarios

---
+++ src/users/user.go
@@ -0,0 +1,19 @@
+package users
+
+type User struct {
+	id int
+	user string
+	name string
+}
+
+func (u User) load() {
+
+}
+
+func (u User) save() {
+
+}
+
+func (u User) remove() {
+	
+}
---
+++ src/users/usermanager.go
@@ -0,0 +1,19 @@
+package users
+
+type UserManager struct {
+
+}
+
+func (u UserManager) getUser(){
+
+}
+
+
+func (u UserManager) newUser(){
+
+}
+
+
+func (u UserManager) deleteUser(){
+	
+}
