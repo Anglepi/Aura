fb0a206eb803edf327d51a1424bd4a4908b320d1
Ángel Píñar Rivas <angle@correo.ugr.es>
2020-11-01 21:27:54 +0100

    #20 #9 Incluir estructura de datos

--- src/aura.go
+++ src/aura.go
@@ -1,7 +1,22 @@
-package aura
+package main
+
+import (
+	"fmt"
+	mm "github.com/Anglepi/Aura/src/memes"
+	db "github.com/Anglepi/Aura/src/auramemory"
+	lg "github.com/Anglepi/Aura/src/logs"
+	nt "github.com/Anglepi/Aura/src/notes"
+	rm "github.com/Anglepi/Aura/src/reminders"
+	us "github.com/Anglepi/Aura/src/users"
+)
 
 type Aura struct {
-
+	memeManager mm.MemeManager
+	database db.AuraMemory
+	logs lg.AuraLogger
+	notes nt.NoteManager
+	reminders rm.ReminderManager
+	users us.UserManager
 }
 
 func (a Aura) listenEvent() {
@@ -18,4 +33,8 @@ func (a Aura) requestUtility() {
 
 func (a Aura) answerEvent() {
 
+}
+
+func main(){
+	fmt.Println("Hola, soy Aura")
 }
